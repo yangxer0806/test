8fe5b5bff396bdba45b6ba113920ee59aaef4ad4
yangxiaoer <xe_young@163.com>
2023-02-08 18:03:03 +0800

    test

--- main2.py
+++ main2.py
@@ -55,16 +55,3 @@ for i in range(len(d)):
  sheet.cell(row=i+2, column=4).value = d[i][3]
 wb2.save('Excel2/all.xlsx')
 
-
-
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
